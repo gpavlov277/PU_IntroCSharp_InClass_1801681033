919b32912b8f90d7696ae2c38b72d9a8fd4d98ad
Georgi Pavlov <gpavlov277@gmail.com>
2018-10-23 16:01:05 +0300

    Week4

---
+++ W3InClass/CandyShop/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+namespace CandyShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+            
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+         
+
+
+            string[] lines = File.ReadAllLines(@"C:\Users\Asus\Desktop\Week3\PU_IntroCSharp_InClass_1801681033\PU_IntroCSharp_InClass_1801681033\W3InClass\CandyShop\files\students.txt");
+           
+            System.Console.WriteLine("Contents of students.txt = ");
+            foreach (string line in lines)
+            {
+                string[] currentElements = line.Split(' ');
+
+
+                string firstName = currentElements[1];
+                string lastName = currentElements[3];
+                string fullName = $"{firstName}{lastName}";
+                int sum = 0;
+
+                for (int i = 0; i < firstName.Length; i++)
+                {
+                    sum += (int)firstName[i];
+
+                }
+
+                Console.WriteLine($"Candy for {fullName}->{sum}");
+            }
+
+        }
+    }
+}
---
+++ W3InClass/CandyShop/System/Encoding.cs
@@ -0,0 +1,9 @@
+﻿using System.Text;
+
+namespace System
+{
+    internal class Encoding
+    {
+        public static Text.Encoding UTF8 { get; internal set; }
+    }
+}
